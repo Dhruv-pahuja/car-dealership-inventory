52a3b1ea3f860900a42af22a80895c1529df3a07
Dhruv Pahuja <dhruvpahuja013@gmail.com>
2026-07-30 13:54:35 +0530

    feat(frontend) :  configure routing for different views

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return (
-   <>
-    <h1>Car inventory system</h1>
-   </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
